c53f88717ebb0359f55bcf2c57dfa930ea9b4d28
cloudaman4-pixel <cloudaman4@gmail.com>
2025-10-30 12:53:58 +0200

    fixed a send file to gemini

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ payload = {
     ]
 }
 
-headers = {"Content-Type": "application/json"}
+headers = {"Content-Type": "application/on"}
 
 response = requests.post(url, headers=headers, data=json.dumps(payload))
 
